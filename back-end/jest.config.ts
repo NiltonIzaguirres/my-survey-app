a55027c064762e3f8ab2370120cb89fab88a102b
Nilton Izaguirres <niltonizaguirres2003@gmail.com>
2024-03-21 08:45:27 -0300

    chore: add jest configs and scripts

--- back-end/jest.config.ts
+++ back-end/jest.config.ts
@@ -2,7 +2,13 @@ import type { Config } from 'jest'
 
 const config: Config = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/**/*-protocols.ts',
+    '!**/protocols/**',
+    '!**/test/**'
+  ],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   transform: {
